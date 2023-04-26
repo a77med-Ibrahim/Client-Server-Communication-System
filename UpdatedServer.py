1da55f04cc994740d0c06e4dc8768b9b78347379
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2023-04-26 17:04:23 +0300

    Add files via upload

---
+++ UpdatedServer.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+from datetime import datetime
+
+HOST = "localhost"  # Standard loopback interface address (localhost)
+PORT = 5555 # Port to listen on (non-privileged ports are > 1023), 5555 instead of 555 is just to allow non-privileged execution
+ADDRESS = (HOST, PORT) # handling the address this way is easier
+USERPASS = "CMPE322\0bilgiuni" # we will send user authentication as a null concatenated string
+
+givenOutPut="You are OK, for asking me 'date, time, capTurkey, quit"
+
+def authenticateClient(clientSocket):
+    # get data from the client
+    data = clientSocket.recv(1024)
+    # return true if the client successfully authenticated
+    return data.decode() == USERPASS
+
+def handleClientRequests(clientSocket):
+    while (True):
+            option = clientSocket.recv(1024)
+            match option.decode():
+                case "date":
+                    message = datetime.now().strftime("\nDate is: %d/%m/%Y ")
+                    clientSocket.sendall(message.encode())
+                case "time":
+                    message = datetime.now().strftime("\nTime is: %H:%M:%S ")
+                    clientSocket.sendall(message.encode())
+                case "capTurkey":
+                    message = "\nThe capital of Turkey is Ankara "
+                    clientSocket.sendall(message.encode())
+                case "quit":
+                    message = "\nBye bye.\n"
+                    clientSocket.sendall(message.encode())
+                    break
+
+def handleClient(clientSocket):
+    try:
+        # Send a welcome message to the client
+        response = "Hello, client!\nPlease send your login information."
+        clientSocket.sendall(response.encode())
+        # continually ask the client to authenticate until it authenticates successfully
+        while (not authenticateClient(clientSocket)):
+            response = "incorrect login information, failed to authenticate, try again."
+            clientSocket.sendall(response.encode())
+        message = f"\n{givenOutPut}"
+        clientSocket.sendall(message.encode())
+        handleClientRequests(clientSocket)
+    except BrokenPipeError:
+        pass
+    clientSocket.close()
+
+# Create a server socket object
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+    serverSocket.bind(ADDRESS)
+    serverSocket.listen()
+    #print(serverSocket)
+    while (True):
+        # Accept a new client connection
+        clientSocket, clientAddress = serverSocket.accept()
+        print(f"Received connection from {clientAddress}")
+
+        # Start a new thread to handle the client connection
+        clientThread = threading.Thread(target=handleClient, args=(clientSocket,))
+        clientThread.start()
